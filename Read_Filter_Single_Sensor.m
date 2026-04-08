5ee24879b27efbefe96d1fbd07fcbf1147550816
Colin Simon Graf <colgr041@student.liu.se>
2026-04-08 18:58:29 +0200

    added moving average filter, performance of Butterworth are still the best

--- Read_Filter_Single_Sensor.m
+++ Read_Filter_Single_Sensor.m
@@ -12,10 +12,12 @@ Fc = 0.5; % Cutoff frequency (val od frequency that acts like treshlod
 Wn = Fc/(Fs/2); % Normalize frequency 
 [b, a] = butter(6, Wn, 'low'); % 4 is the order of the filter , b and a are transfer functions coefficients
 y = filtfilt(b, a, pressure);
+% y = movmean(pressure, 3); % moving average filter
 
 % Plot without filter
 figure;
 plot(time, pressure, '-o', 'DisplayName','Pressure difference');
+ylim([30 40]);
 grid on
 xlabel('Time [ms]');
 ylabel('Pressure difference [Pa]');
@@ -25,6 +27,7 @@ legend
 % Plot with filter
 figure;
 plot(time, y, '-o', 'DisplayName','Pressure difference');
+ylim([30 40]);
 grid on
 xlabel('Time [ms]');
 ylabel('Pressure difference [Pa]');
