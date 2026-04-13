876f3b668aa66e2b9e79120151d648d64fda4c84
Colin Simon Graf <colgr041@student.liu.se>
2026-04-13 20:08:50 +0200

    added moving median filter

--- Read_Filter_Single_Sensor.m
+++ Read_Filter_Single_Sensor.m
@@ -1,5 +1,5 @@
 %% Reader and plotter of the .csv
-one_sensor = 'prova.txt';
+one_sensor = 'Test-yawprobe.txt';
 
 data = readmatrix(one_sensor, 'NumHeaderLines', 20);
 
@@ -8,16 +8,17 @@ pressure = data(:,2);
 
 %% Butterwhorth filter
 Fs = 10; % Sampling frequency
-Fc = 0.5; % Cutoff frequency (val od frequency that acts like treshlod
+Fc = 0.1; % Cutoff frequency (val od frequency that acts like treshlod
 Wn = Fc/(Fs/2); % Normalize frequency 
 [b, a] = butter(6, Wn, 'low'); % 4 is the order of the filter , b and a are transfer functions coefficients
 y = filtfilt(b, a, pressure);
-% y = movmean(pressure, 3); % moving average filter
+% y = movmean(pressure, 10); % moving average filter
+% y = movmedian(pressure, 20); % moving average filter
 
 % Plot without filter
 figure;
 plot(time, pressure, '-o', 'DisplayName','Pressure difference');
-ylim([30 40]);
+ylim([-1000 1000]);
 grid on
 xlabel('Time [ms]');
 ylabel('Pressure difference [Pa]');
@@ -27,7 +28,7 @@ legend
 % Plot with filter
 figure;
 plot(time, y, '-o', 'DisplayName','Pressure difference');
-ylim([30 40]);
+ylim([-1000 1000]);
 grid on
 xlabel('Time [ms]');
 ylabel('Pressure difference [Pa]');
