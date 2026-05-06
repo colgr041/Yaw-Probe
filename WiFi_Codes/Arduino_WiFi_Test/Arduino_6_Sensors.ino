c4f022be62958b8ab1cde59f1302e00f352284d5
Colin Simon Graf <colgr041@student.liu.se>
2026-05-06 19:15:13 +0200

    6 sensor code fixed

--- WiFi_Codes/Arduino_WiFi_Test/Arduino_6_Sensors.ino
+++ WiFi_Codes/Arduino_WiFi_Test/Arduino_6_Sensors.ino
@@ -3,23 +3,26 @@
 const char* WIFI_SSID     = "Hotspot Di Leo";
 const char* WIFI_PASSWORD = "Pablinaa";
 
-const String scriptId = "AKfycbxE_sUwXWHQ3hDi8FSbz7GFitqB_oUvsKnhC_mb9QbFsoPlSex9I8ZLZfTi9aFJ7a_u";
+// Use your working Script ID here
+const String scriptId = "AKfycbwv9RFOYNjLV6F6g3CUYPKRrHGMfxH_fRbu3cSkxhsYp9RVLMWqVUEPcWmLfMCb6rI";
 const char* host = "script.google.com";
 
 const int sensorPins[6] = {A0, A1, A2, A3, A4, A5};
 
-float refVolt  = 5000.0;
+// Your specific calibration values
+float paOffset = 15.26;
+float refVolt  = 5000.0; 
 float vPerUnit = refVolt / 1024.0;
+float paPerMV  = 2500.0 / (4500.0 - 500.0);
 
 // Timing
-unsigned long sampleInterval = 1000;
+unsigned long sampleInterval = 500; 
 unsigned long nextSampleTime = 0;
 
-// Batch
+// Batch settings
 const int batchSize = 5;
+float pressureBuffer[6][batchSize]; // 6 sensors, 5 samples each
 unsigned long timeBuffer[batchSize];
-float sensorBuffer[6][batchSize]; // [sensor][sample]
-
 int bufferIndex = 0;
 
 WiFiSSLClient client;
@@ -42,15 +45,17 @@ void loop() {
   if (millis() >= nextSampleTime) {
     nextSampleTime += sampleInterval;
 
-    timeBuffer[bufferIndex] = nextSampleTime;
-
+    // Record data for all 6 sensors
     for (int s = 0; s < 6; s++) {
-      float mV = analogRead(sensorPins[s]) * vPerUnit;
-      sensorBuffer[s][bufferIndex] = mV; // raw voltage (or convert if needed)
+      float mV = (analogRead(sensorPins[s]) * vPerUnit);
+      float pa = ((mV - 500.0) * paPerMV) - 1250 + paOffset;
+      pressureBuffer[s][bufferIndex] = pa;
     }
-
-    Serial.println("Buffered sample " + String(bufferIndex));
+    
+    timeBuffer[bufferIndex] = nextSampleTime;
     bufferIndex++;
+
+    Serial.println("Sampled " + String(bufferIndex) + "/" + String(batchSize));
 
     if (bufferIndex >= batchSize) {
       sendBatch();
@@ -62,45 +67,42 @@ void loop() {
 void sendBatch() {
   if (client.connect(host, 443)) {
 
+    // 1. Build Time string
     String timeData = "";
-    String sData[6] = {"", "", "", "", "", ""};
-
     for (int i = 0; i < batchSize; i++) {
       timeData += String(timeBuffer[i]);
+      if (i < batchSize - 1) timeData += ",";
+    }
 
-      for (int s = 0; s < 6; s++) {
-        sData[s] += String(sensorBuffer[s][i], 2);
-        if (i < batchSize - 1) {
-          sData[s] += ",";
-        }
-      }
-
-      if (i < batchSize - 1) {
-        timeData += ",";
+    // 2. Build Sensor strings
+    String pData[6] = {"", "", "", "", "", ""};
+    for (int s = 0; s < 6; s++) {
+      for (int i = 0; i < batchSize; i++) {
+        pData[s] += String(pressureBuffer[s][i], 2);
+        if (i < batchSize - 1) pData[s] += ",";
       }
     }
 
-    String url = "/macros/s/" + scriptId + "/exec?";
-    url += "time=" + timeData;
-    url += "&s0=" + sData[0];
-    url += "&s1=" + sData[1];
-    url += "&s2=" + sData[2];
-    url += "&s3=" + sData[3];
-    url += "&s4=" + sData[4];
-    url += "&s5=" + sData[5];
+    // 3. Construct URL
+    String url = "/macros/s/" + scriptId + "/exec?time=" + timeData;
+    for (int s = 0; s < 6; s++) {
+      url += "&p" + String(s) + "=" + pData[s];
+    }
 
+    // 4. Send HTTP Request
     client.println("GET " + url + " HTTP/1.1");
     client.println("Host: script.google.com");
+    client.println("User-Agent: Arduino/1.0");
     client.println("Connection: close");
     client.println();
 
-    Serial.println("Sent batch (6 sensors)");
+    Serial.println("Batch sent to Google Sheets");
 
+    // Clean up connection
     unsigned long timeout = millis();
     while (client.connected() && millis() - timeout < 2000) {
       while (client.available()) client.read();
     }
-
     client.stop();
 
   } else {
